ea744af0590c213600c1bedd73d3a54b72d94026
Jason Robert Fincham <JRFincham@yahoo.com>
2015-03-04 16:07:42 -0500

    List controller added

---
+++ app/scripts/lists/lists.controller.js
@@ -0,0 +1,14 @@
+;(function() {
+  "use strict";
+
+  angular.module('FaveBands')
+
+  .controller('ListController', ['$scope',
+
+    function($scope){
+
+
+
+  }]);
+
+}());
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('JavaScript!!!');
+;(function() {
+  "use strict";
+
+  angular.module('FaveBands', ['ngRoute'])
+
+  .constant('PARSE',{
+
+    URL: 'https://api.parse.com/1/',
+    CONFIG: {
+      headers: {
+          'X-Parse-Application-Id' : 'NFvg7B0BGuGM6GOjTxoWW4R4nE2Hpk1jKtDEMqkr',
+          'X-Parse-REST-API-Key' : 'peAT0JC3ZIRhPIUYv11mHvjc97vVZ0xQ7hPVD3Yv'
+      }
+
+    }
+
+  })
+
+  .config(['$routeProvider', function($routeProvider){
+
+    $routeProvider
+
+    .when('/', {
+      templateUrl: 'scripts/lists/lists.home.temp.html',
+      controller: 'ListController'
+    });
+
+    
+
+  }]);
+
+}());
